74d1ccb7c1bc2d62c08e125797e33ae841af5da0
AimanYosofi <=>
2025-06-14 09:37:53 +0300

    init commit

---
+++ app/src/main/java/com/sanaa/tudee_assistant/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.sanaa.tudee_assistant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}

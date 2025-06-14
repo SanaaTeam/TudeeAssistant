292f96fc354c0056f0f676cedfbb2068ed0c97b3
AimanYosofi <=>
2025-06-14 09:57:12 +0300

    add theme include colors and text style

---
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/text_style/TudeeTextStyle.kt
@@ -0,0 +1,19 @@
+package com.sanaa.tudee_assistant.presentation.design_system.text_style
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+
+data class TudeeTextStyle(
+    val headline: SizedTextStyle,
+    val title: SizedTextStyle,
+    val body: SizedTextStyle,
+    val label: SizedTextStyle,
+)
+
+data class SizedTextStyle(
+    val large: TextStyle,
+    val medium: TextStyle,
+    val small: TextStyle,
+)
+
+val LocalTudeeTextStyle = staticCompositionLocalOf { defaultTextStyle }
---
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/theme/TudeeTheme.kt
@@ -0,0 +1,24 @@
+package com.sanaa.tudee_assistant.presentation.design_system.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import com.sanaa.tudee_assistant.presentation.design_system.color.LocalTudeeColors
+import com.sanaa.tudee_assistant.presentation.design_system.color.darkSchemaColors
+import com.sanaa.tudee_assistant.presentation.design_system.color.lightSchemaColors
+import com.sanaa.tudee_assistant.presentation.design_system.text_style.LocalTudeeTextStyle
+import com.sanaa.tudee_assistant.presentation.design_system.text_style.defaultTextStyle
+
+@Composable
+fun TudeeTheme(
+    isDarkTheme: Boolean = false,
+    content: @Composable () -> Unit,
+) {
+    val theme = if (isDarkTheme) darkSchemaColors else lightSchemaColors
+
+    CompositionLocalProvider(
+        LocalTudeeColors provides theme,
+        LocalTudeeTextStyle provides defaultTextStyle
+    ) {
+        content()
+    }
+}

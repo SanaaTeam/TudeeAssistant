2dc06b8e5dd7ef1f5a03a3b90515925494193cfc
Radwa85 <radwa.mohamed5033@gmail.com>
2025-06-16 01:44:04 +0300

    use wrapContentHeight instead of fixed height

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/component/BottomSheet.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/component/BottomSheet.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -17,17 +18,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.sanaa.tudee_assistant.presentation.design_system.theme.Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseBottomSheet(
+    modifier: Modifier = Modifier,
     sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
     content: @Composable () -> Unit,
-    sheetHeight: Dp,
-    modifier: Modifier = Modifier,
     onDismiss: () -> Unit,
 ) {
     ModalBottomSheet(
@@ -54,7 +53,7 @@ fun BaseBottomSheet(
         },
         content = {
             Box(
-                modifier = Modifier.height(sheetHeight),
+                modifier = Modifier.wrapContentHeight(),
             )
             { content() }
         }

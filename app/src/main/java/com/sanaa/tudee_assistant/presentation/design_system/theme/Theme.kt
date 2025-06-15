a5c879ad7b839b30c839aad6f9d90c558b8b16c6
AimanYosofi <=>
2025-06-15 20:55:22 +0300

    add dimension to the design system and apply it to the current branch

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/theme/Theme.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/theme/Theme.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import com.sanaa.tudee_assistant.presentation.design_system.color.LocalTudeeColors
 import com.sanaa.tudee_assistant.presentation.design_system.color.TudeeColors
+import com.sanaa.tudee_assistant.presentation.design_system.dimension.LocalTudeeDimension
+import com.sanaa.tudee_assistant.presentation.design_system.dimension.TudeeDimension
 import com.sanaa.tudee_assistant.presentation.design_system.text_style.LocalTudeeTextStyle
 import com.sanaa.tudee_assistant.presentation.design_system.text_style.TudeeTextStyle
 
@@ -13,4 +15,7 @@ object Theme {
 
     val textStyle: TudeeTextStyle
         @Composable @ReadOnlyComposable get() = LocalTudeeTextStyle.current
+
+    val dimension: TudeeDimension
+        @Composable @ReadOnlyComposable get() = LocalTudeeDimension.current
 }

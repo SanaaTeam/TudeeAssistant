84cfc3c4fa8c41fe980a3a07eb1813941cf7e23d
Mohammed ALjabouri <eng.moha100@gmail.com>
2025-06-15 17:52:34 +0300

    add splash screen and font

---
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/theme/Font.kt
@@ -0,0 +1,18 @@
+package com.sanaa.tudee_assistant.presentation.design_system.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.sanaa.tudee_assistant.R
+
+val Nunito = FontFamily(
+    Font(R.font.nunito_extra_light, weight = FontWeight.ExtraLight),
+    Font(R.font.nunito_light, weight = FontWeight.Light),
+    Font(R.font.nunito_regular, weight = FontWeight.Normal),
+    Font(R.font.nunito_medium, weight = FontWeight.Medium),
+    Font(R.font.nunito_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.nunito_bold, weight = FontWeight.Bold),
+)
+val cherry_bomb = FontFamily(
+    Font(R.font.cherry_bomb_one_regular, FontWeight.Normal)
+)

8c002851400cdea16853b73a2e291d53f2537e84
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-06-16 15:30:58 +0300

    save logoName and appBar captions in String resources

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/component/ِAppBar.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/component/ِAppBar.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,8 +30,6 @@ import com.sanaa.tudee_assistant.presentation.design_system.theme.TudeeTheme
 
 @Composable
 fun AppBar(
-    title: String,
-    caption: String,
     modifier: Modifier = Modifier,
     tailComponent: @Composable () -> Unit = {},
 ) {
@@ -56,14 +55,14 @@ fun AppBar(
 
             Column {
                 Text(
-                    text = title,
+                    text = stringResource(R.string.logoName),
                     fontFamily = CherryBomb,
                     fontWeight = FontWeight(400),
                     fontSize = 18.sp,
                     color = Theme.color.onPrimary,
                 )
                 Text(
-                    text = caption,
+                    text = stringResource(R.string.appBarCaption),
                     style = Theme.textStyle.label.small,
                     color = Theme.color.onPrimaryCaption
                 )
@@ -83,8 +82,6 @@ private fun AppBarLightPreview() {
     TudeeTheme(isDarkTheme = isDarkMode) {
 
         AppBar(
-            title = "Tudee",
-            caption = "Your cute Helper for Every Task",
             tailComponent = {
                 DarkModeThemeSwitchButton(
                     isDarkMode,
@@ -103,8 +100,6 @@ private fun AppBarDarkPreview() {
     TudeeTheme(isDarkTheme = isDarkMode) {
 
         AppBar(
-            title = "Tudee",
-            caption = "Your cute Helper for Every Task",
             tailComponent = {
                 DarkModeThemeSwitchButton(
                     isDarkMode,

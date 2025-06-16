50678c6bc97df62168d4823336dbd8e499504ee9
raouf-kamel <raoufkamel2013@gmail.com>
2025-06-16 19:36:36 +0300

    rename MyApplication to TudeeAssistantApplication

--- app/src/main/java/com/sanaa/tudee_assistant/TudeeAssistantApplication.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/TudeeAssistantApplication.kt
@@ -3,19 +3,21 @@ package com.sanaa.tudee_assistant
 import android.app.Application
 import com.sanaa.tudee_assistant.di.databaseModule
 import com.sanaa.tudee_assistant.di.servicesModule
+import com.sanaa.tudee_assistant.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MyApplication : Application() {
+class TudeeAssistantApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidContext(this@MyApplication)
+            androidContext(this@TudeeAssistantApplication)
             modules(
                 databaseModule,
                 servicesModule,
+                viewModelModule,
             )
         }
     }

8fbbf693cff25167b59710fe3bd2c347f7edfc3e
haider <haideraljawasim@gmail.com>
2025-06-17 23:13:35 +0300

    add getTaskCountByCategoryId

--- app/src/main/java/com/sanaa/tudee_assistant/domain/service/CategoryService.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/domain/service/CategoryService.kt
@@ -4,8 +4,8 @@ import com.sanaa.tudee_assistant.domain.model.Category
 import kotlinx.coroutines.flow.Flow
 
 interface CategoryService {
-    fun getCategoryById(categoryId: Int): Category
-    suspend fun getCategories(): Flow<List<Category>>
+    suspend fun getCategoryById(categoryId: Int): Category
+    suspend fun getAllCategories(): Flow<List<Category>>
     suspend fun addCategory(category: Category)
     suspend fun updateCategory(category: Category)
     suspend fun deleteCategoryById(categoryId: Int)

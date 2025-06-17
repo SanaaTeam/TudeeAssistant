842c40409e011ce984fc3ec1e7f2b4ef285f8460
haider <haideraljawasim@gmail.com>
2025-06-17 23:29:14 +0300

    remove suspend keyword from getAllGategories

--- app/src/main/java/com/sanaa/tudee_assistant/domain/service/TaskService.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/domain/service/TaskService.kt
@@ -2,6 +2,7 @@ package com.sanaa.tudee_assistant.domain.service
 
 import com.sanaa.tudee_assistant.domain.model.Task
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
 
 interface TaskService {
     suspend fun addTask(task: Task)
@@ -13,5 +14,5 @@ interface TaskService {
     fun getAllTasks(): Flow<List<Task>>
     fun getTasksByCategoryId(categoryId: Int): Flow<List<Task>>
     fun getTasksByStatus(status: Task.TaskStatus): Flow<List<Task>>
-    fun getTasksByDueDate(dueDate: String): Flow<List<Task>>
+    fun getTasksByDueDate(dueDate: LocalDate): Flow<List<Task>>
 }

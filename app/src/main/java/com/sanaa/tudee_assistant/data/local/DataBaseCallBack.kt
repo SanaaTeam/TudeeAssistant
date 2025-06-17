5f22fe10167a33b5d00f1032ec53897093f27fc3
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-06-18 00:05:45 +0300

    delete unnecessary imports and clean up

--- app/src/main/java/com/sanaa/tudee_assistant/data/local/DataBaseCallBack.kt
+++
@@ -1,8 +0,0 @@
-package com.sanaa.tudee_assistant.data.local
-
-import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
--- app/src/main/java/com/sanaa/tudee_assistant/di/DatabaseModule.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/di/DatabaseModule.kt
@@ -1,17 +1,13 @@
 package com.sanaa.tudee_assistant.di
 
-
-import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.sanaa.tudee_assistant.data.local.AppDatabase
-import com.sanaa.tudee_assistant.data.local.dto.CategoryLocalDto
 import com.sanaa.tudee_assistant.data.utils.getDefaultCategories
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.koin.compose.getKoin
 import org.koin.dsl.module
 
 private const val DATABASE_NAME = "tudee.db"
@@ -41,52 +37,3 @@ val databaseModule = module {
     single { get<AppDatabase>().categoryDao() }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//internal fun provideDatabase(context: Context): AppDatabase {
-//    return Room.databaseBuilder(
-//        context,
-//        AppDatabase::class.java,
-//        DATABASE_NAME
-//    )
-//        .build()
-//}
-//
-//internal fun provideTaskDao(db: AppDatabase): TaskDao = db.taskDao()
-//internal fun provideCategoryDao(db: AppDatabase): CategoryDao = db.categoryDao()
-//
-//val databaseModule = module {
-//    single { provideDatabase(get()) }
-//    single { provideTaskDao(get()) }
-//    single { provideCategoryDao(get()) }
-//}
-
-
-
-
-
-//
-//class DatabaseCallback(
-//    private val context: Context
-//) : RoomDatabase.Callback() {
-//
-//    override fun onCreate(db: SupportSQLiteDatabase) {
-//        super.onCreate(db)
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val dao = AppDatabase.getDatabase(context).categoryDao()
-//            dao.insertAll(getDefaultCategories())
-//        }
-//    }

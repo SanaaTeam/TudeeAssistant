23c133a77f4973c753967c8bd9dd06bcf6506aba
EL-MANCY <abdelrahmanelmancy@gmail.com>
2025-06-17 23:35:19 +0300

    process the selected image to bitmap

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screens/AddNewCategorey.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screens/AddNewCategorey.kt
@@ -1,6 +1,11 @@
 package com.sanaa.tudee_assistant.presentation.screens
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,12 +18,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.scale
 import com.sanaa.tudee_assistant.R
 import com.sanaa.tudee_assistant.presentation.design_system.component.BaseBottomSheet
 import com.sanaa.tudee_assistant.presentation.design_system.component.TudeeTextField
@@ -28,7 +36,9 @@ import com.sanaa.tudee_assistant.presentation.design_system.component.button.Sec
 import com.sanaa.tudee_assistant.presentation.design_system.theme.Theme
 import com.sanaa.tudee_assistant.presentation.design_system.theme.dropShadowColor
 import com.sanaa.tudee_assistant.presentation.utils.dropShadow
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,6 +50,11 @@ fun AddNewCategory(
     var showBottomSheet by remember { mutableStateOf(true) }
     var categoryTitle by remember { mutableStateOf("") }
     var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+    var processedImageBytes by remember { mutableStateOf<Bitmap?>(null) }
+
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
 
     if (showBottomSheet) {
         BaseBottomSheet(
@@ -76,6 +91,13 @@ fun AddNewCategory(
                             onImageSelected = { uri ->
                                 selectedImageUri = uri
                                 onImageSelected(uri)
+                                if (uri != null) {
+                                    scope.launch {
+                                        processedImageBytes = processImage(context, uri)
+                                    }
+                                } else {
+                                    processedImageBytes = null
+                                }
                             }
                         )
                     }
@@ -92,18 +114,17 @@ fun AddNewCategory(
                     ) {
                         PrimaryButton(
                             label = stringResource(R.string.add),
-                            enabled = selectedImageUri != null && categoryTitle.isNotBlank(),
+                            enabled = processedImageBytes != null && categoryTitle.isNotBlank(),
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(bottom = Theme.dimension.regular),
-                            onClick = onAddClick
+                            onClick = { onAddClick() }
                         )
                         SecondaryButton(
                             lable = stringResource(R.string.cancel),
                             modifier = Modifier.fillMaxWidth(),
                             onClick = { showBottomSheet = false },
-
-                            )
+                        )
                     }
                 }
             },
@@ -116,4 +137,20 @@ fun AddNewCategory(
 @Composable
 fun AddNewCategoryScreenPreview() {
     AddNewCategory({}, {})
+}
+
+suspend fun processImage(context: Context, uri: Uri): Bitmap? {
+    return withContext(Dispatchers.IO) {
+        try {
+            val originalBitmap = if (Build.VERSION.SDK_INT < 28) {
+                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+            } else {
+                val source = ImageDecoder.createSource(context.contentResolver, uri)
+                ImageDecoder.decodeBitmap(source)
+            }
+            originalBitmap.scale(32, 32)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 }

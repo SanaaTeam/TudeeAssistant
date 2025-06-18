764372d22db49674c909d448b10e643a7cee6046
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-06-18 22:14:23 +0300

    fix swipe to delete in TaskListColumn

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/taskScreen/TaskListColumn.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/taskScreen/TaskListColumn.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -27,7 +31,7 @@ import com.sanaa.tudee_assistant.presentation.model.TaskPriority
 @Composable
 fun TaskListColumn(
     taskList: List<CategoryTaskState>,
-    onTaskSwipe: () -> Boolean = { true }
+    onTaskSwipe: (Int) -> Boolean = { true }
 ) {
 
     LazyColumn(
@@ -40,11 +44,11 @@ fun TaskListColumn(
         ),
         verticalArrangement = Arrangement.spacedBy(Theme.dimension.small)
     ) {
-        items(taskList) { task ->
+        itemsIndexed(taskList) { index, task ->
             val dismissState = rememberSwipeToDismissBoxState(
                 confirmValueChange = { dismissValue ->
                     if (dismissValue == SwipeToDismissBoxValue.EndToStart) {
-                        onTaskSwipe()
+                        onTaskSwipe(index)
                     } else false
                 }
             )
@@ -101,5 +105,11 @@ private fun TaskListColumnPreview() {
             priority = TaskPriority.HIGH
         ),
     )
-    TaskListColumn(items)
+    var itemsState by remember { mutableStateOf(items) }
+
+    TaskListColumn(itemsState, onTaskSwipe = { index ->
+        itemsState = itemsState.filterNot { item -> item == itemsState[index] }
+         false
+    }
+    )
 }

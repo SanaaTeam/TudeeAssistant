c886ee248e2661e1fdf3cf30e38f55f83f9bdefb
shahad-97 <shos8406@gmail.com>
2025-06-18 20:21:10 +0300

    Add pages  and animation  in indicator and  button To go to the next page on onboarding_screen

--- app/src/main/java/com/sanaa/tudee_assistant/screen/OnBoardingScreen.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/screen/OnBoardingScreen.kt
@@ -1,111 +1,211 @@
 package com.sanaa.tudee_assistant.screen
 
+import android.app.Activity
+import android.view.WindowManager
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sanaa.tudee_assistant.R
-import com.sanaa.tudee_assistant.presentation.design_system.theme.Nunito
+import com.sanaa.tudee_assistant.presentation.design_system.component.button.FloatingActionButton
 import com.sanaa.tudee_assistant.presentation.design_system.theme.Theme
 import com.sanaa.tudee_assistant.presentation.design_system.theme.TudeeTheme
-
+import kotlinx.coroutines.launch
 
 @Composable
-fun OnBoardingScreen(
-    modifier: Modifier = Modifier
-) {
+fun OnBoardingScreen(modifier: Modifier = Modifier) {
+    val isInPreview = LocalView.current.isInEditMode
+
+    if (!isInPreview) {
+        val window = LocalView.current.context as Activity
+        LaunchedEffect(Unit) {
+            window.window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+    }
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .background(Theme.color.overlay)
-    )
-
-    {
-
-        Box(
-            modifier = Modifier.fillMaxSize()
-
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.boarding_screen000),
-                contentDescription = null,
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .size(614.dp, 584.dp)
-            )
-
-        }
-        Text(
-            text = stringResource(R.string.skip),
-            fontSize = 18.sp,
-            fontFamily = Nunito,
-            color = Theme.color.primary,
-            lineHeight = 19.sp,
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 52.dp)
-
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.boarding_screen000),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxSize()
         )
-        val pagerState = rememberPagerState(
-            pageCount = { 3 },
-            initialPage = 0,
 
+        val pagerState = rememberPagerState(pageCount = { 3 }, initialPage = 0)
+
+        if (pagerState.currentPage != 2) {
+            Text(
+                text = stringResource(R.string.skip),
+                color = Theme.color.primary,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .padding(top = 16.dp, start = 16.dp)
+                    .systemBarsPadding()
             )
-        HorizontalPager(
+        }
 
-            state = pagerState,
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.weight(1f)
             ) {
-            Box(
-                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-            ) {
-                when (it) {
-                    0 -> DialogContainer(
-                        title = "Overwhelmed with tasks?",
-                        description = "Let’s bring some order to the chaos.\nTudee is here to help you sort, plan, and\nbreathe easier.",
-                        painter = painterResource(id = R.drawable.boarding_screen1)
-                    )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    when (it) {
+                        0 -> DialogContainer(
+                            title = "Overwhelmed with tasks?",
+                            description = "Let’s bring some order to the chaos.\nTudee is here to help you sort, plan, and\nbreathe easier.",
+                            painter = painterResource(id = R.drawable.boarding_screen1),
+                            pagerState = pagerState
 
-                    1 -> DialogContainer(
-                        title = "Overwhelmed with tasks?",
-                        description = "Let’s bring some order to the chaos.\nTudee is here to help you sort, plan, and\nbreathe easier.",
-                        painter = painterResource(id = R.drawable.boarding_screen2)
-                    )
+                        )
 
-                    2 -> DialogContainer(
-                        title = "Overwhelmed with tasks?",
-                        description = "Let’s bring some order to the chaos.\nTudee is here to help you sort, plan, and\nbreathe easier.",
-                        painter = painterResource(id = R.drawable.boarding_screen3)
-                    )
+                        1 -> DialogContainer(
+                            title = "Uh-oh! Procrastinating again",
+                            description = "Tudee not mad… just a little \ndisappointed, Let’s get back on track \ntogether.",
+                            painter = painterResource(id = R.drawable.boarding_screen2),
+                            pagerState = pagerState
 
+                        )
+
+                        2 -> DialogContainer(
+                            title = "Let’s complete task and celebrate\ntogether.",
+                            description = "Progress is progress. Tudee will celebrate you on for every win, big or small.",
+                            painter = painterResource(id = R.drawable.boarding_screen3),
+                            pagerState = pagerState
+
+                        )
+                    }
                 }
             }
+            PageIndicator(currentPage = pagerState.currentPage, pageCount = pagerState.pageCount)
         }
-
     }
 }
 
-@Preview()
+@Composable
+fun PageIndicator(currentPage: Int, pageCount: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(bottom = 16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        repeat(pageCount) { index ->
+            val animatedColor by animateColorAsState(
+                targetValue = if (index == currentPage) Theme.color.primary else Color(0xFFEFF9FE),
+                animationSpec = tween(durationMillis = 100),
+                label = ""
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 4.dp, vertical = 24.dp)
+                    .size(width = 100.dp, height = 5.dp)
+                    .background(animatedColor, shape = RoundedCornerShape(2.dp))
+            )
+        }
+    }
+}
+
+@Composable
+fun DialogContainer(
+    title: String,
+    description: String,
+    painter: Painter,
+    pagerState: PagerState,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(width = 296.dp, height = 260.dp)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(192.dp)
+                .background(Theme.color.onPrimaryCard, RoundedCornerShape(32.dp))
+                .border(1.dp, Theme.color.onPrimaryStroke, RoundedCornerShape(32.dp))
+                .padding(horizontal = 16.dp)
+                .padding(top = 24.dp, bottom = 48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = Theme.textStyle.title.medium,
+                color = Theme.color.title,
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(
+                text = description,
+                style = Theme.textStyle.body.medium,
+                color = Theme.color.body,
+                textAlign = TextAlign.Center,
+            )
+        }
+
+        val coroutineScope = rememberCoroutineScope()
+        FloatingActionButton(
+            enabled = true,
+            isLoading = false,
+            onClick = {
+                val nextPage = pagerState.currentPage + 1
+                if (nextPage < pagerState.pageCount) {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(nextPage)
+                    }
+                }
+            },
+            iconRes = R.drawable.icon_arrow_right_double,
+            modifier = Modifier.offset(y = (-70).dp)
+        )
+    }
+}
+
+@Preview(widthDp = 360)
 @Composable
 private fun BoardingScreenPreview() {
     TudeeTheme(isDarkTheme = false) {
@@ -113,58 +213,8 @@ private fun BoardingScreenPreview() {
             modifier = Modifier
                 .fillMaxSize()
                 .background(Theme.color.surface)
-
         ) {
             OnBoardingScreen()
-
-        }
-    }
-}
-
-@Composable
-fun DialogContainer(
-    title: String, description: String, painter: Painter,
-
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 24.dp, vertical = 24.dp),
-
-
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(32.dp)
-    )
-
-    {
-        Image(
-            painter = painter,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.size(width = 296.dp, height = 250.dp)
-        )
-
-        Column(
-            modifier = Modifier
-                .background(Theme.color.onPrimaryCard, RoundedCornerShape(32.dp))
-                .border(1.dp, Theme.color.onPrimaryStroke, RoundedCornerShape(32.dp))
-                .padding(horizontal = 16.dp, vertical = 24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-
-            Text(
-                text = title,
-                style = Theme.textStyle.title.medium,
-                color = Theme.color.title,
-            )
-            Text(
-                text = description,
-                color = Theme.color.body,
-                style = Theme.textStyle.body.medium,
-                textAlign = TextAlign.Center,
-            )
         }
     }
 }

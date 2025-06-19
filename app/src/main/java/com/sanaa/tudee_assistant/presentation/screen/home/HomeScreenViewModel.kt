350d6014966cb36c9c2793cdceae28c1f33b7d27
AimanYosofi <=>
2025-06-19 23:51:02 +0300

    get data for the home screen

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/home/HomeScreenViewModel.kt
@@ -1,14 +1,49 @@
 package com.sanaa.tudee_assistant.presentation.screen.home
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sanaa.tudee_assistant.domain.service.CategoryService
+import com.sanaa.tudee_assistant.domain.service.TaskService
 import com.sanaa.tudee_assistant.presentation.model.TaskUiStatus
+import com.sanaa.tudee_assistant.presentation.model.mapper.toState
 import com.sanaa.tudee_assistant.presentation.state.TaskUiState
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import com.sanaa.tudee_assistant.presentation.utils.BaseViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class HomeScreenViewModel : ViewModel() {
-    private val _state = MutableStateFlow(HomeScreenUiState())
-    val state = _state.asStateFlow()
+class HomeScreenViewModel(
+    private val taskService: TaskService,
+    private val categoryService: CategoryService,
+) : BaseViewModel<HomeScreenUiState>(HomeScreenUiState()) {
+
+    init {
+        getCategories()
+        getTasks()
+    }
+
+    private fun getTasks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskService.getAllTasks().collect { tasks ->
+                _state.update { state ->
+                    state.copy(
+                        tasks = tasks.map { it.toState() }
+                    )
+                }
+            }
+        }
+    }
+
+    private fun getCategories() {
+        viewModelScope.launch {
+            categoryService.getCategories().collect { categories ->
+                _state.update { state ->
+                    state.copy(
+                        categories = categories.map { it.toState() }
+                    )
+                }
+            }
+        }
+    }
 
     fun onAddTask() {
 

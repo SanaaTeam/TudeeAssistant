0f800f46c8e7b9bcbf3e900662f763bfc5b0dd45
Radwa85 <radwa.mohamed5033@gmail.com>
2025-06-19 23:22:49 +0300

    add TaskFormViewModel to the di module.

--- app/src/main/java/com/sanaa/tudee_assistant/di/ViewModelModule.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.sanaa.tudee_assistant.di
 
+import com.sanaa.tudee_assistant.presentation.screen.addEditScreen.TaskFormViewModel
 import com.sanaa.tudee_assistant.presentation.screen.taskScreen.TaskViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -7,5 +8,6 @@ import org.koin.dsl.module
 val viewModelModule = module {
 
 viewModel { TaskViewModel(get(), get()) }
+ viewModel { TaskFormViewModel(get(), get())  }
 
 }

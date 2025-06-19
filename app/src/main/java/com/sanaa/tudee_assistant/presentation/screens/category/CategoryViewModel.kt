f44546f284241cf3f846508493a7f93738e9d995
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-06-19 14:29:50 +0300

    update category view model

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screens/category/CategoryViewModel.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screens/category/CategoryViewModel.kt
@@ -3,12 +3,17 @@ package com.sanaa.tudee_assistant.presentation.screens.category
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sanaa.tudee_assistant.domain.service.CategoryService
+import com.sanaa.tudee_assistant.domain.service.TaskService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class CategoryViewModel() : ViewModel() {
+class CategoryViewModel(
+    private val categoryService: CategoryService,
+    private val taskService: TaskService
+) : ViewModel() {
 
     private val _state = MutableStateFlow(CategoryUiState())
     val state: StateFlow<CategoryUiState>
@@ -18,26 +23,25 @@ class CategoryViewModel() : ViewModel() {
         loadCategoriesWithTasks()
     }
 
-
     private fun loadCategoriesWithTasks() {
         viewModelScope.launch {
-            try {
-                TODO()
+            _state.update { it.copy(isLoading = true) }
+
+            categoryService.getCategories().collect { categoryList ->
+                val mappedList = categoryList.map { category ->
+                    val taskCount = taskService
+                        .getTaskCountByCategoryId(category.id ?: 0)
+                        .first()
+                    category.toUiState(taskCount)
+                }
 
                 _state.update {
                     it.copy(
-
+                        currentDateCategory = mappedList, isLoading = false
                     )
                 }
-            } catch (e: Exception) {
-                null
             }
         }
     }
-
-    fun getTaskCount(categoryId: Int): Int {
-        return TODO()
-    }
-
 
 }

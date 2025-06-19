65ea6497fa4a7dab643773d1aa628721ed138ff3
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-06-19 17:49:25 +0300

    apply , theme manger to tuddee theme composable

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/theme/TudeeTheme.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/theme/TudeeTheme.kt
@@ -3,18 +3,33 @@ package com.sanaa.tudee_assistant.presentation.design_system.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.sanaa.tudee_assistant.data.utils.ThemeManager
 import com.sanaa.tudee_assistant.presentation.design_system.color.LocalTudeeColors
 import com.sanaa.tudee_assistant.presentation.design_system.color.darkSchemaColors
 import com.sanaa.tudee_assistant.presentation.design_system.color.lightSchemaColors
 import com.sanaa.tudee_assistant.presentation.design_system.text_style.LocalTudeeTextStyle
 import com.sanaa.tudee_assistant.presentation.design_system.text_style.defaultTextStyle
+import org.koin.compose.koinInject
 
 @Composable
 fun TudeeTheme(
-    isDarkTheme: Boolean = false,
+    themeManager: ThemeManager= koinInject(),
     content: @Composable () -> Unit,
 ) {
-    val theme = if (isDarkTheme) darkSchemaColors else lightSchemaColors
+    var isDark by remember { mutableStateOf(false) }
+    LaunchedEffect(themeManager) {
+        themeManager.isDarkTheme.collect {
+            isDark = it
+        }
+    }
+
+    val theme = if (isDark) darkSchemaColors else lightSchemaColors
 
     CompositionLocalProvider(
         LocalTudeeColors provides theme,

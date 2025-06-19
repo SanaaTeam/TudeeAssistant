594a29cd6790a44e8b5e2367520a8bb9344325de
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-06-19 17:50:21 +0300

    wrapping app with tuddee theme

--- app/src/main/java/com/sanaa/tudee_assistant/MainActivity.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.sanaa.tudee_assistant.presentation.TudeeApp
+import com.sanaa.tudee_assistant.presentation.design_system.theme.TudeeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TudeeApp()
+            TudeeTheme {
+                TudeeApp()
+            }
         }
     }
 }

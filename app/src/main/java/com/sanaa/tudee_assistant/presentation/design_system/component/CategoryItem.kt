ad2e6fa5cba4ee8757c778429f2f6a4d4585c540
Radwa85 <radwa.mohamed5033@gmail.com>
2025-06-19 17:26:25 +0300

    removed unused imports

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/component/CategoryItem.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/design_system/component/CategoryItem.kt
@@ -11,21 +11,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.sanaa.tudee_assistant.R
 import com.sanaa.tudee_assistant.data.utils.getAssetsImagePainter
 import com.sanaa.tudee_assistant.domain.model.Category
 import com.sanaa.tudee_assistant.presentation.design_system.theme.Theme
 import com.sanaa.tudee_assistant.presentation.design_system.theme.TudeeTheme
-import com.sanaa.tudee_assistant.presentation.model.CategoryState
 
 @Composable
 fun CategoryItem(

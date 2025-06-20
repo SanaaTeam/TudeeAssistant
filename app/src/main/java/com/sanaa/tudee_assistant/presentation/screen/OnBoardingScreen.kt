498a8db1261bae5d38f6b3540a0012f7c5f9e12d
AimanYosofi <=>
2025-06-20 07:58:25 +0300

    fix issue caused by merge

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/OnBoardingScreen.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/OnBoardingScreen.kt
@@ -8,13 +8,26 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -33,7 +46,6 @@ import com.sanaa.tudee_assistant.R
 import com.sanaa.tudee_assistant.presentation.design_system.component.button.FloatingActionButton
 import com.sanaa.tudee_assistant.presentation.design_system.theme.Theme
 import com.sanaa.tudee_assistant.presentation.design_system.theme.TudeeTheme
-import com.sanaa.tudee_assistant.presentation.screen.main.MainScreen
 import kotlinx.coroutines.launch
 
 data class OnBoardingContent(
@@ -199,8 +211,15 @@ fun DialogContainer(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(192.dp)
-                .background(Theme.color.onPrimaryCard, RoundedCornerShape(Theme.dimension.extraLarge))
-                .border(1.dp, Theme.color.onPrimaryStroke, RoundedCornerShape(Theme.dimension.extraLarge))
+                .background(
+                    Theme.color.onPrimaryCard,
+                    RoundedCornerShape(Theme.dimension.extraLarge)
+                )
+                .border(
+                    1.dp,
+                    Theme.color.onPrimaryStroke,
+                    RoundedCornerShape(Theme.dimension.extraLarge)
+                )
                 .padding(horizontal = Theme.dimension.medium)
                 .padding(top = Theme.dimension.large, bottom = 48.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -241,7 +260,7 @@ fun DialogContainer(
 @Preview(widthDp = 360)
 @Composable
 private fun BoardingScreenPreview() {
-    TudeeTheme(isDarkTheme = false) {
+    TudeeTheme(isDark = false) {
         Box(
             modifier = Modifier
                 .fillMaxSize()

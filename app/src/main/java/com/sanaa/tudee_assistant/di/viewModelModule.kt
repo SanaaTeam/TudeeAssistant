0ca080bbf2dc60661d25b3eea245eb29be99bb72
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-06-20 15:24:08 +0300

    resolve conflict

--- app/src/main/java/com/sanaa/tudee_assistant/di/viewModelModule.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/di/viewModelModule.kt
@@ -2,16 +2,14 @@ package com.sanaa.tudee_assistant.di
 
 import com.sanaa.tudee_assistant.presentation.screen.add_edit_screen.TaskFormViewModel
 import com.sanaa.tudee_assistant.presentation.screen.home.HomeScreenViewModel
-import com.sanaa.tudee_assistant.presentation.screens.category.CategoryViewModel
-import com.sanaa.tudee_assistant.presentation.screen.add_edit_screen.TaskFormViewModel
 import com.sanaa.tudee_assistant.presentation.screen.taskScreen.TaskViewModel
+import com.sanaa.tudee_assistant.presentation.screens.category.CategoryViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
- viewModel { TaskFormViewModel(get(), get())  }
-
+    viewModel { TaskFormViewModel(get(), get()) }
     viewModel { TaskViewModel(get(), get()) }
     viewModel { CategoryViewModel(get(), get(), get()) }
     viewModel { HomeScreenViewModel(get(), get()) }

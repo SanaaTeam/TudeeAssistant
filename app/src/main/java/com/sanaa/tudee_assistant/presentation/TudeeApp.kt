fb50eded05275d177273552da039594f3d240766
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-06-20 14:23:51 +0300

    resolve conflict

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/TudeeApp.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/TudeeApp.kt
@@ -3,7 +3,6 @@ package com.sanaa.tudee_assistant.presentation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,24 +13,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
-import com.sanaa.tudee_assistant.R
 import com.sanaa.tudee_assistant.domain.PreferencesManager
-import com.sanaa.tudee_assistant.presentation.design_system.theme.Theme
 import com.sanaa.tudee_assistant.presentation.design_system.theme.TudeeTheme
 import com.sanaa.tudee_assistant.presentation.route.CategoryTasksScreenRoute
 import com.sanaa.tudee_assistant.presentation.route.HomeScreenRoute
 import com.sanaa.tudee_assistant.presentation.route.MainScreenRoute
 import com.sanaa.tudee_assistant.presentation.route.OnBoardingScreenRoute
 import com.sanaa.tudee_assistant.presentation.route.SplashScreenRoute
-import com.sanaa.tudee_assistant.presentation.route.TasksScreenRoute
 import com.sanaa.tudee_assistant.presentation.screen.OnBoardingScreen
-import com.sanaa.tudee_assistant.presentation.screen.home.HomeScreen
 import com.sanaa.tudee_assistant.presentation.screen.main.MainScreen
 import kotlinx.coroutines.launch
-import org.koin.compose.koinInject
-import com.sanaa.tudee_assistant.presentation.screen.main.composable
-import com.sanaa.tudee_assistant.presentation.screen.taskScreen.TasksScreen
 
 @Composable
 fun TudeeApp(isDarkTheme: Boolean, preferencesManager: PreferencesManager, isFirstLaunch: Boolean) {

379dfbd4fb18de9fa0b709d5b990c548d0ae572d
haider <haideraljawasim@gmail.com>
2025-06-23 00:16:16 +0300

    add CategoryTaskInteractionListener class

---
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/categoryTask/CategoryTaskInteractionListener.kt
@@ -0,0 +1,8 @@
+package com.sanaa.tudee_assistant.presentation.screen.categoryTask
+
+interface CategoryTaskInteractionListener {
+    fun onDeleteClicked()
+    fun onEditClicked()
+    fun onDismissClicked()
+    fun onConfirmClicked()
+}

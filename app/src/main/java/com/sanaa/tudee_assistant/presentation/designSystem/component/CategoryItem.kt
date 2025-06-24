bcf72504398dd894aab174ad45e1c2819ebb89c9
haider <haideraljawasim@gmail.com>
2025-06-24 21:19:28 +0300

    clip the interaction to have circal shape on CategoryItem

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/designSystem/component/CategoryItem.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/designSystem/component/CategoryItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sanaa.tudee_assistant.presentation.composable.CategoryThumbnail
@@ -40,17 +41,18 @@ fun CategoryItem(
                 .size(78.dp),
         ) {
             Box(
-                modifier = Modifier.align(Alignment.TopEnd),
-            ) {
-                topContent()
-            }
-            Box(
                 modifier = Modifier
                     .fillMaxSize()
+                    .clip(RoundedCornerShape(100.dp))
                     .clickable { onClick() },
                 contentAlignment = Alignment.Center,
             ) {
                 CategoryThumbnail(modifier = Modifier.size(32.dp), imagePath = category.imagePath)
+            }
+            Box(
+                modifier = Modifier.align(Alignment.TopEnd),
+            ) {
+                topContent()
             }
         }
 

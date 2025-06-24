a1e7a98f1ae14bb5f4213b84083ff3cae6d4e597
Hager Abobakr <hagarabobakr1@gmail.com>
2025-06-24 18:47:17 +0300

    fix text width in slider

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/designSystem/component/Slider.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/designSystem/component/Slider.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -63,6 +64,7 @@ fun Slider(
             }
 
             Text(
+                modifier = Modifier.fillMaxWidth(0.8f),
                 text = when (status) {
                     TudeeUiStatus.GOOD -> {
                         stringResource(R.string.good_status_message)

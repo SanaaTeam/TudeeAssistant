39142c471a1ad7d604274062221b1364828a0493
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-06-27 01:17:56 +0300

    fix, task service impl test

--- app/src/test/java/com/sanaa/tudee_assistant/data/services/TasksServiceImplTest.kt
+++ app/src/test/java/com/sanaa/tudee_assistant/data/services/TasksServiceImplTest.kt
@@ -154,20 +154,21 @@ class TasksServiceImplTest {
             assertThat(result.isFailure).isTrue()
         }
 
-//    @Test
-//    fun `getTaskById should invoke getTaskById in taskDao with correct taskId`() = runTest {
-//        // Given
-//        coEvery { taskDao.getTaskById(any()) } returns fakeTasks[0]
-//        // When
-//        tasksService.getTaskById(fakeTasks[0].taskId)
-//        // Then
-//        coVerify(exactly = 1) { taskDao.getTaskById(fakeTasks[0].taskId) }
-//    }
+    @Test
+    fun `getTaskById should invoke getTaskById in taskDao with correct taskId`() = runTest {
+        val taskFlow = flowOf(fakeTasks[0])
+        // Given
+        coEvery { taskDao.getTaskById(any()) } returns taskFlow
+        // When
+        tasksService.getTaskById(fakeTasks[0].taskId)
+        // Then
+        coVerify(exactly = 1) { taskDao.getTaskById(fakeTasks[0].taskId) }
+    }
 
 //    @Test
 //    fun `getTaskById should throw TaskNotFoundException when getTaskById returns null`() = runTest {
 //        // Given
-//        coEvery { taskDao.getTaskById(any()) } returns null
+//        coEvery { taskDao.getTaskById(any()) } returns flowOf()
 //        // When
 //        val result = runCatching { tasksService.getTaskById(fakeTasks[0].taskId) }
 //        // Then

0adbad39a11a0cbdc8657574b877cb7f78f23de5
EL-MANCY <abdelrahmanelmancy@gmail.com>
2025-06-26 16:06:51 +0300

    unify strings

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/home/HomeScreenInteractionsListener.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/home/HomeScreenInteractionsListener.kt
@@ -3,27 +3,22 @@ package com.sanaa.tudee_assistant.presentation.screen.home
 import com.sanaa.tudee_assistant.presentation.model.TaskUiState
 
 interface HomeScreenInteractionsListener {
-    // Theme and Snack-bar
     fun onToggleColorTheme()
     fun onHideSnackBar()
 
-    // Task Details Sheet
     fun onTaskClick(taskUiState: TaskUiState)
     fun onDismissTaskDetails()
 
-    // Add Task Sheet
     fun onShowAddTaskSheet()
     fun onHideAddTaskSheet()
     fun onAddTaskSuccess()
     fun onAddTaskError(errorMessage: String)
 
-    // Edit Task Sheet
     fun onShowEditTaskSheet(taskToEdit: TaskUiState)
     fun onHideEditTaskSheet()
     fun onEditTaskSuccess()
     fun onEditTaskError(errorMessage: String)
 
-    // Status Update from Details
     fun onMoveStatusSuccess()
     fun onMoveStatusFail()
 }

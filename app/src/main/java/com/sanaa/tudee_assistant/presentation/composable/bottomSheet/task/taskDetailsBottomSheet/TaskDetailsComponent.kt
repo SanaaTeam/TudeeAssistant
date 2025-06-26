5a3347d6d1aa980357c857df452ff8dbeb2a1bcd
AimanYosofi <=>
2025-06-26 19:02:43 +0300

    use asStateWithLifeCycle instead on collect as state only inside the screens

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/composable/bottomSheet/task/taskDetailsBottomSheet/TaskDetailsComponent.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/composable/bottomSheet/task/taskDetailsBottomSheet/TaskDetailsComponent.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sanaa.tudee_assistant.R
 import com.sanaa.tudee_assistant.presentation.composable.CategoryThumbnail
 import com.sanaa.tudee_assistant.presentation.designSystem.component.BaseBottomSheet
@@ -45,7 +45,7 @@ fun TaskDetailsComponent(
     onMoveStatusSuccess: (TaskUiStatus) -> Unit = {},
     onMoveStatusFail: () -> Unit = {},
 ) {
-    val state: State<DetailsUiState> = interactionListener.state.collectAsState()
+    val state: State<DetailsUiState> = interactionListener.state.collectAsStateWithLifecycle()
 
     val changeStatusTo = when (state.value.status) {
         TaskUiStatus.TODO -> stringResource(R.string.mark_as_in_progress)

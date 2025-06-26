4e0294bcf6c83f19bef4b6b980f3e3ae00e368e2
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-06-26 10:38:32 +0300

    test OnBoardingViewModel

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/onBoarding/OnBoardingScreen.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/onBoarding/OnBoardingScreen.kt
@@ -47,7 +47,6 @@ import com.sanaa.tudee_assistant.presentation.model.OnBoardingPageContentItem
 import com.sanaa.tudee_assistant.presentation.navigation.AppNavigation
 import com.sanaa.tudee_assistant.presentation.navigation.MainScreenRoute
 import com.sanaa.tudee_assistant.presentation.navigation.OnBoardingScreenRoute
-import com.sanaa.tudee_assistant.presentation.utils.DataProvider
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -273,13 +272,32 @@ private fun PageIndicator(
 @Composable
 private fun BoardingScreenPreview() {
 
+    val pages = listOf(
+        OnBoardingPageContentItem(
+            title = R.string.onboarding_title_0,
+            description = R.string.onboarding_desc_0,
+            imageRes = R.drawable.robot_onboarding_page0
+        ),
+        OnBoardingPageContentItem(
+            title = R.string.onboarding_title_1,
+            description = R.string.onboarding_desc_1,
+            imageRes = R.drawable.robot_onboarding_page1
+        ),
+        OnBoardingPageContentItem(
+            title = R.string.onboarding_title_2,
+            description = R.string.onboarding_desc_2,
+            imageRes = R.drawable.robot_onboarding_page2
+        ),
+    )
+
+
     val isDarkTheme = false
 
     var state by remember {
         mutableStateOf(
             OnBoardingScreenUiState(
                 isDarkTheme = isDarkTheme,
-                pageList = DataProvider.getOnBoardingPageContent(),
+                pageList = pages,
                 currentPageIndex = 0
             )
         )
--- app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/onBoarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/screen/onBoarding/OnBoardingViewModel.kt
@@ -1,9 +1,10 @@
 package com.sanaa.tudee_assistant.presentation.screen.onBoarding
 
 import androidx.lifecycle.viewModelScope
+import com.sanaa.tudee_assistant.R
 import com.sanaa.tudee_assistant.domain.service.PreferencesManager
+import com.sanaa.tudee_assistant.presentation.model.OnBoardingPageContentItem
 import com.sanaa.tudee_assistant.presentation.utils.BaseViewModel
-import com.sanaa.tudee_assistant.presentation.utils.DataProvider
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -18,7 +19,7 @@ class OnBoardingViewModel(
                 _state.update {
                     it.copy(
                         isDarkTheme = isDarkTheme,
-                        pageList = DataProvider.getOnBoardingPageContent(),
+                        pageList = getOnBoardingPageContent(),
                         currentPageIndex = 0
                     )
                 }
@@ -35,7 +36,7 @@ class OnBoardingViewModel(
     }
 
     override fun onSkipClick() {
-            _state.update { it.copy(isSkipable = true) }
+        _state.update { it.copy(isSkipable = true) }
 
         viewModelScope.launch {
             preferencesManager.setOnboardingCompleted()
@@ -44,5 +45,25 @@ class OnBoardingViewModel(
 
     override fun setCurrentPage(pageIndex: Int) {
         _state.update { it.copy(currentPageIndex = pageIndex) }
+    }
+
+    private fun getOnBoardingPageContent(): List<OnBoardingPageContentItem> {
+        return listOf(
+            OnBoardingPageContentItem(
+                title = R.string.onboarding_title_0,
+                description = R.string.onboarding_desc_0,
+                imageRes = R.drawable.robot_onboarding_page0
+            ),
+            OnBoardingPageContentItem(
+                title = R.string.onboarding_title_1,
+                description = R.string.onboarding_desc_1,
+                imageRes = R.drawable.robot_onboarding_page1
+            ),
+            OnBoardingPageContentItem(
+                title = R.string.onboarding_title_2,
+                description = R.string.onboarding_desc_2,
+                imageRes = R.drawable.robot_onboarding_page2
+            ),
+        )
     }
 }

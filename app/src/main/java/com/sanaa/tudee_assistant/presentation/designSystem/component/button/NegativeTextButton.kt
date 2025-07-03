c8eca9f7d029aee7fdb09965203e14e1de1a2870
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-03 10:42:01 +0300

    animate NegativeTextButton background color

--- app/src/main/java/com/sanaa/tudee_assistant/presentation/designSystem/component/button/NegativeTextButton.kt
+++ app/src/main/java/com/sanaa/tudee_assistant/presentation/designSystem/component/button/NegativeTextButton.kt
@@ -1,11 +1,15 @@
 package com.sanaa.tudee_assistant.presentation.designSystem.component.button
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -21,11 +25,10 @@ fun NegativeTextButton(
     isLoading: Boolean = false,
     onClick: () -> Unit,
 ) {
-    val contentColor = when (enabled) {
-        true -> Theme.color.error
-        false -> Theme.color.stroke
-    }
-
+    val animatedContentColor by animateColorAsState(
+        targetValue = if(enabled)Theme.color.error else Theme.color.stroke,
+        label ="animatedContentColor"
+    )
     Row(
         modifier
             .clickable(
@@ -39,7 +42,7 @@ fun NegativeTextButton(
             label = label,
             enabled = enabled,
             isLoading = isLoading,
-            contentColor = contentColor
+            contentColor = animatedContentColor
         )
     }
 }
